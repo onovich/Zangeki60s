f139776ff3a206ab9f931474726a6dbda10da52e
onovich <hjn1110@163.com>
2024-05-07 22:37:22 +0800

    <feature> add: 受击转向; 受击后移

--- Assets/Scripts_Runtime/Business_Game/Domains/GameRoleDomain.cs
+++ Assets/Scripts_Runtime/Business_Game/Domains/GameRoleDomain.cs
@@ -47,6 +47,7 @@ namespace Zangeki {
             if (target.allyStatus == AllyStatus.Player) {
                 UIApp.GameInfo_PlayHurtAnim(ctx.uiContext, target.hp);
                 target.RoleMod?.PlayHit();
+                target.Move_SetFace(-role.faceDir);
             }
 
             if (target.hp <= 0) {
